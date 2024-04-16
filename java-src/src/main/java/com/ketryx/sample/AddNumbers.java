755bddf7875fe595fcba7458382f3d9e3a948117
Jan Pöschko <jan@ketryx.com>
2024-04-16 22:05:27 +0200

    Add item annotations to Java files

--- java-src/src/main/java/com/ketryx/sample/AddNumbers.java
+++ java-src/src/main/java/com/ketryx/sample/AddNumbers.java
@@ -1,5 +1,10 @@
 package com.ketryx.sample;
 
+/**
+ * Utility class to add two numbers.
+ *
+ * @itemId:AddNumbers
+ */
 public class AddNumbers {
     public static int addNumbers(int a, int b) {
         return a + b;
--- java-src/src/test/java/com/ketryx/sample/AddNumbersTest.java
+++ java-src/src/test/java/com/ketryx/sample/AddNumbersTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AddNumbersTest {
+    /**
+     * Tests that two numbers can be added correctly.
+     * @tests:AddNumbers
+     */
     @Test
     public void addNumbersTest() {
         assertEquals(3, AddNumbers.addNumbers(1, 2));
