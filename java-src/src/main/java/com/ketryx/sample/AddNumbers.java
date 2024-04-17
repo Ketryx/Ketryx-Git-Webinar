5e3d4e79eb9839e5828399c3997000a227e83f88
Gabriel Pascualy <gabrielp@ketryx.com>
2024-04-16 21:50:43 -0400

    Update AddNumbers.java

--- java-src/src/main/java/com/ketryx/sample/AddNumbers.java
+++ java-src/src/main/java/com/ketryx/sample/AddNumbers.java
@@ -4,6 +4,7 @@ package com.ketryx.sample;
  * Utility class to add two numbers.
  *
  * @itemId:AddNumbers
+ * @itemTitle:"Dose calculation"
  */
 public class AddNumbers {
     public static int addNumbers(int a, int b) {
