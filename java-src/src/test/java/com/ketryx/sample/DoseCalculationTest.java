28d7d51f2e870260fc1cd6bbfa6e1e2b64088394
Milan Dzenovljanovic <miland@ketryx.com>
2024-10-29 13:54:08 +0100

    Update DoseCalculationTest.java to update the TC title

--- java-src/src/test/java/com/ketryx/sample/DoseCalculationTest.java
+++ java-src/src/test/java/com/ketryx/sample/DoseCalculationTest.java
@@ -8,7 +8,7 @@ public class DoseCalculationTest {
     /**
      * Tests that dose is computed correctly.
      * @tests:DoseCalculation
-     * @itemTitle:"Test Dose Calculation"
+     * @itemTitle:"Verify Dose Calculation"
      */
     @Test
     public void doseCalculationTest() {
